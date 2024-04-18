8f7c82613c32d1b8face54b861202ba3b6e7a5f5
Mbah Neilvani <neilvanimbah@gmail.com>
2024-04-18 22:47:28 +0100

    updated the code

--- Js/Num_in_word.js
+++ Js/Num_in_word.js
@@ -44,3 +44,24 @@ function switchItUp(number){
       case 9: return 'Nine';
     }
   }
+
+  function switchItUp(number){
+    //Write your own Code!
+    var a = number;
+    var b = {
+    0: "Zero",
+    1: "One",
+    2: "Two",
+    3: "Three",
+    4: "Four",
+    5: "Five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine"
+    };
+    return b[a];
+    
+    
+    
+    }
